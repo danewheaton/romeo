bd90ff5b69645ab68528693e1e446eea381529f8
redflamegard <corey.king@loop.colum.edu>
2017-03-14 00:05:36 -0500

    Fixed

---
+++ FixedRomeo/Assets/Scripts/DemoScripts/Gun.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour
+{
+	public Rigidbody2D energyBlast, energyBlast2, energyBlast3;				// Prefab of the rocket.
+	public float speed = 20f;				// The speed the rocket will fire at.
+
+
+	private PlayerControl playerCtrl;		// Reference to the PlayerControl script.
+	private Animator anim;					// Reference to the Animator component.
+
+    float chargeAmount = 0;
+
+    void Awake()
+	{
+		// Setting up the references.
+		anim = transform.root.gameObject.GetComponent<Animator>();
+		playerCtrl = transform.root.GetComponent<PlayerControl>();
+
+        print(anim.gameObject.name);
+	}
+
+
+	void Update ()
+	{
+        if (Input.GetButton("Fire1"))
+        {
+            chargeAmount += Time.deltaTime;
+        }
+
+        if (chargeAmount > .2f && chargeAmount < .21f) anim.SetTrigger("Charge");
+
+        Rigidbody2D projectile;
+        if (chargeAmount > 1) projectile = energyBlast3;
+        else if (chargeAmount > .5f) projectile = energyBlast2;
+        else projectile = energyBlast;
+
+		// If the fire button is pressed...
+		if(Input.GetButtonUp("Fire1"))
+		{
+			// ... set the animator Shoot trigger parameter and play the audioclip.
+			anim.SetTrigger("Shoot");
+			GetComponent<AudioSource>().Play();
+
+			// If the player is facing right...
+			if(playerCtrl.facingRight)
+			{
+				// ... instantiate the rocket facing right and set it's velocity to the right. 
+				Rigidbody2D bulletInstance = Instantiate(projectile, transform.position, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
+				bulletInstance.velocity = new Vector2(speed, 0);
+			}
+			else
+			{
+				// Otherwise instantiate the rocket facing left and set it's velocity to the left.
+				Rigidbody2D bulletInstance = Instantiate(projectile, transform.position, Quaternion.Euler(new Vector3(0,0,180f))) as Rigidbody2D;
+				bulletInstance.velocity = new Vector2(-speed, 0);
+			}
+
+            chargeAmount = 0;
+		}
+	}
+}
