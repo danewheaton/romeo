e12908c6fb0aabeb3efb750998e0c7ee4c5d6a27
danewheaton <wheaton.dane@gmail.com>
2017-03-15 15:24:51 -0500

    deleting scripts

---
+++ FixedRomeo/Assets/Scripts/DemoScripts/Explosion.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Invoke("SelfDestruct", .2f);
+    }
+
+    void SelfDestruct()
+    {
+        Destroy(gameObject);
+    }
+}
