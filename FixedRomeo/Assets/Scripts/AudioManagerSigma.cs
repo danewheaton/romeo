df500b7b4a57e12b568dc269967cebbcfd57bf19
chris-s-harper <chris.harper@loop.colum.edu>
2017-04-09 14:11:17 -0500

    I can't wait for this to break
    Adjusted the audio sources/manager for Sigma, made cutscene

--- FixedRomeo/Assets/Scripts/AudioManagerSigma.cs
+++ FixedRomeo/Assets/Scripts/AudioManagerSigma.cs
@@ -8,18 +8,32 @@ public class AudioManagerSigma : MonoBehaviour
     [SerializeField]
     private AudioClip[] audioClips = new AudioClip[7];
 
+    [SerializeField]
     private AudioSource audioSource;
+    [SerializeField]
+    private AudioSource shootingAudioSource;
 
 	// Use this for initialization
 	void Start ()
     {
-        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+        if (Input.GetButtonDown("Jump"))
+        {
+            PlayJump();
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            PlayCharge();
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            shootingAudioSource.Stop();
+        }
 	}
 
     //Each sound has a different function, in case anything wants to be done with things like the audio mixer, reverb, etc
@@ -30,73 +44,71 @@ public class AudioManagerSigma : MonoBehaviour
         audioSource.Play();
     }
 
-    private void PlayShoot()
-    {
-        //Shoot is the second element in the audioclips array
-        //TODO: Find good way to sort through "shoot" clip variants
-        audioSource.clip = audioClips[1];
-        audioSource.Play();
-    }
-
     private void PlayCharge()
     {
         //TODO: Play "windup" charge sound effect, then proceed to loop through a second sound effect of "being charged"
-    }
-
-    private void PlaySteps()
-    {
-        //TODO: Make sure each step is properly slightly varried for each "step"
+        shootingAudioSource.clip = audioClips[1];
+        shootingAudioSource.loop = true;
+        shootingAudioSource.Play();
     }
 
     private void PlayWallClimb()
     {
-        //WallClimb is the fifth element in the audioclips array
-        audioSource.clip = audioClips[4];
+        //WallClimb is the third element in the audioclips array
+        audioSource.clip = audioClips[2];
         audioSource.Play();
     }
 
     private void PlayDamage()
     {
-        //Damage is the sixth element in the audioclips array
-        audioSource.clip = audioClips[5];
+        //Damage is the fourth element in the audioclips array
+        audioSource.clip = audioClips[3];
         audioSource.Play();
     }
 
     private void PlayDie()
     {
-        //Die is the seventh element in the audioclips array
-        audioSource.clip = audioClips[7];
+        //Die is the fifth element in the audioclips array
+        audioSource.clip = audioClips[4];
         audioSource.Play();
     }
 
     //function can be called with an argument of which desired sound must be played
-    public void PlaySound(string soundToPlay)
+    //public void PlaySound(string soundToPlay)
+    //{
+    //    switch (soundToPlay)
+    //    {
+    //        case "jump":
+    //            PlayJump();
+    //            break;
+    //        case "shoot":
+    //            PlayShoot();
+    //            break;
+    //        case "charge":
+    //            PlayCharge();
+    //            break;
+    //        case "step":
+    //            PlaySteps();
+    //            break;
+    //        case "wallclimb":
+    //            PlayWallClimb();
+    //            break;
+    //        case "damage":
+    //            PlayDamage();
+    //            break;
+    //        case "die":
+    //            PlayDie();
+    //            break;
+    //        default:
+    //            break;
+    //    }
+    //}
+
+    private void OnCollisionEnter2D(Collision2D collider)
     {
-        switch (soundToPlay)
+        if (collider.gameObject.tag == "Enemy")
         {
-            case "jump":
-                PlayJump();
-                break;
-            case "shoot":
-                PlayShoot();
-                break;
-            case "charge":
-                PlayCharge();
-                break;
-            case "step":
-                PlaySteps();
-                break;
-            case "wallclimb":
-                PlayWallClimb();
-                break;
-            case "damage":
-                PlayDamage();
-                break;
-            case "die":
-                PlayDie();
-                break;
-            default:
-                break;
+            PlayDamage();
         }
     }
 }
