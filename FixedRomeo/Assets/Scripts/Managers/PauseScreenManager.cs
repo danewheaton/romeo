2bef024cb024a9e836dffdd7e9e93e850e618bd7
CHRIS-PC\cshar <chris.harper@loop.colum.edu>
2017-04-23 17:11:05 -0500

    Added Title and Level Select Themes. Implemented Pause Screen Prefabs, all that needs to be done is dragged into the canvas above the main UI panels,have the continue button be put in the event system first object field, and have the Pause button be "Pause". Didn't drag it in myself in case someone was working on that scene. Added very basic health pickup functionality

--- FixedRomeo/Assets/Scripts/Managers/PauseScreenManager.cs
+++ FixedRomeo/Assets/Scripts/Managers/PauseScreenManager.cs
@@ -2,35 +2,51 @@
 using System.Collections;
 using System;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
+//All code by CH unless otherwise mentioned
+//goes on the PauseScreen canvas
 public class PauseScreenManager : MonoBehaviour
 {
     [SerializeField]
     private string pauseButton;
 
-    private bool isPaused = false;
-	
-	// Update is called once per frame
-	void Update ()
+    private GameObject PausePanel;
+
+    void Start()
+    {
+        PausePanel = GameObject.Find("PausePanel");
+        AudioListener.pause = false;
+        PausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
         Pause();
-	}
+    }
 
     private void Pause()
     {
         if (Input.GetButtonDown(pauseButton))
         {
-            isPaused = !isPaused;
-            if (isPaused)
-            {
-                Debug.Log("Game is paused");
-                Time.timeScale = 0;
-            }
-            if (isPaused)
-            {
-                Debug.Log("Game unpaused");
-                Time.timeScale = 1;
-            }
+            PausePanel.SetActive(true);
+            AudioListener.pause = true;
+            Time.timeScale = 0;
         }
+    }
+
+    //functions to quit game and continue through the pause screen
+    public void Continue()
+    {
+        PausePanel.SetActive(false);
+        AudioListener.pause = false;
+        Time.timeScale = 1;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
---
+++ FixedRomeo/Assets/Scripts/Objects/HealthPickup.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPickup : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+
+            //if player health is not already at 100
+            if (true)
+            {
+                playerHealth.TakeDamage(-3);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
